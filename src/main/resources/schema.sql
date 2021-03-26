14b029128daa620ff2e8967c4003b39939b60712
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2021-03-26 15:55:52 +0530

    Removing unused files and deprecated postman collection

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE file_server_token
-(
-    file_token character varying  NOT NULL,
-    user_token character varying  NOT NULL,
-    validity_date timestamp without time zone NOT NULL,
-    server_id character varying  NOT NULL
-)
--- src/test/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE file_server_token
-(
-    file_token character varying  NOT NULL,
-    user_token character varying  NOT NULL,
-    validity_date timestamp without time zone NOT NULL,
-    server_id character varying  NOT NULL
-);
-
-INSERT into file_server_token VALUES('fileServerToken','testing_key',current_timestamp,'example.com');
