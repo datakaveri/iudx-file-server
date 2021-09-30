80a0762bbdb16c1ed2a12bc5dc51df2b28fd18ce
Kailash Adhikari <adhikari.kailash88@gmail.com>
2021-09-30 16:52:46 +0530

    patch

--- src/main/java/iudx/file/server/authenticator/AuthenticationVerticle.java
+++ src/main/java/iudx/file/server/authenticator/AuthenticationVerticle.java
@@ -1,9 +1,13 @@
 package iudx.file.server.authenticator;
 
-import static iudx.file.server.authenticator.utilities.Constants.*;
+import static iudx.file.server.common.Constants.AUTH_SERVICE_ADDRESS;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.serviceproxy.ServiceBinder;
 import iudx.file.server.common.WebClientFactory;
 import iudx.file.server.common.service.CatalogueService;
@@ -17,6 +21,7 @@ public class AuthenticationVerticle extends AbstractVerticle {
   private static final String authAddress = AUTH_SERVICE_ADDRESS;
   private ServiceBinder binder;
   private MessageConsumer<JsonObject> consumer;
+  private AuthenticationService jwtAuthenticationService;
 
 
   @Override
@@ -24,13 +29,31 @@ public class AuthenticationVerticle extends AbstractVerticle {
 
     webClientFactory = new WebClientFactory(vertx, config());
     catalogueService = new CatalogueServiceImpl(vertx, webClientFactory, config());
-    auth = new AuthenticationServiceImpl(vertx, catalogueService, webClientFactory, config());
+   // auth = new AuthenticationServiceImpl(vertx, catalogueService, webClientFactory, config());
+    
+    JWTAuthOptions jwtAuthOptions = new JWTAuthOptions();
+    jwtAuthOptions.addPubSecKey(
+        new PubSecKeyOptions()
+            .setAlgorithm("ES256")
+            .setBuffer("-----BEGIN PUBLIC KEY-----\n" +
+                "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE8BKf2HZ3wt6wNf30SIsbyjYPkkTS\n" +
+                "GGyyM2/MGF/zYTZV9Z28hHwvZgSfnbsrF36BBKnWszlOYW0AieyAUKaKdg==\n" +
+                "-----END PUBLIC KEY-----\n" +
+                ""));
+    
+    jwtAuthOptions.getJWTOptions().setIgnoreExpiration(true);
+    JWTAuth jwtAuth = JWTAuth.create(vertx, jwtAuthOptions);
+
+    //@TODO: replace binder with jwt once auth server available.
+    jwtAuthenticationService =
+        new JwtAuthenticationServiceImpl(vertx, jwtAuth,  config(),catalogueService);
+    
 
     binder = new ServiceBinder(vertx);
 
     consumer =
         binder.setAddress(authAddress)
-            .register(AuthenticationService.class, auth);
+            .register(AuthenticationService.class, jwtAuthenticationService);
 
   }
 
