f061454e66beaf3cf061b9b14ba9ab6458031c2c
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2022-12-22 13:30:40 +0530

    Update file

--- src/main/java/iudx/file/server/authenticator/authorization/AuthorizationRequest.java
+++ src/main/java/iudx/file/server/authenticator/authorization/AuthorizationRequest.java
@@ -21,32 +21,6 @@ public final class AuthorizationRequest {
     return api;
   }
 
-//  @Override
-//  public int hashCode() {
-//    final int prime = 31;
-//    int result = 1;
-//    result = prime * result + ((api == null) ? 0 : api.hashCode());
-//    result = prime * result + ((method == null) ? 0 : method.hashCode());
-//    return result;
-//  }
-//
-//  @Override
-//  public boolean equals(Object obj) {
-//    if (this == obj)
-//      return true;
-//    if (obj == null)
-//      return false;
-//    if (getClass() != obj.getClass())
-//      return false;
-//    AuthorizationRequest other = (AuthorizationRequest) obj;
-//    if (api != other.api)
-//      return false;
-//    if (method != other.method)
-//      return false;
-//    return true;
-//  }
-
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
