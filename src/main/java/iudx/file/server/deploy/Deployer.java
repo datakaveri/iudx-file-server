144a7eda5a17672fbe6a940861b28438baed2a38
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-01-20 11:45:47 +0530

    Udpate Deployer

--- src/main/java/iudx/file/server/deploy/Deployer.java
+++ src/main/java/iudx/file/server/deploy/Deployer.java
@@ -120,6 +120,11 @@ public class Deployer {
       return;
     }
 
+    //get common configs and add this to config object
+    JsonObject commonConfigs=configs.getJsonObject("commonConfig");
+    config.mergeIn(commonConfigs, true);
+
+
     int numInstances = config.getInteger("verticleInstances");
     vertx.deployVerticle(moduleName,
         new DeploymentOptions().setInstances(numInstances).setConfig(config), ar -> {
